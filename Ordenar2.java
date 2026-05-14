f70168432bf9e1bc14d9d2e226adb4ea31d83b32
ferrazciro <imblack122@gmail.com>
2026-05-14 01:07:24 +0000

    aula dia 13/05

--- Ordenar2.java
+++ Ordenar2.java
@@ -1,23 +1,23 @@
 void main(){
 
-    var nome = new Strin[5];
+    var nome = new String[5];
     String aux;
 
     for (int cont=0;cont<nome.length;cont++)
-        nome[cont] = IO.readln("Digite o nome[" + cont + "]: " );
+         nome[cont] = IO.readln("Digite o nome[" + cont + "]: ");
 
     IO.println("Estes foram os nomes digitados: ");
     for (int cont=0;cont<nome.length;cont++)
-        IO.println("Nome[" + (cont) + "]: " + nome[cont]);
+         IO.println("Nome[" + (cont) + "]: " + nome[cont]);
     //ROTINA DE ORDENAÇÃO
     for (int i=0;i<nome.length-1;i++)
-        for (int j=i+1;j<nome.length;j++)
-            if (nome[i].compareToIgnoreCase(nome[j]) > 0){
+         for (int j=i+1;j<nome.length;j++)
+              if (nome[i].compareToIgnoreCase(nome[j]) > 0){
                 aux = nome[i];
                 nome[i]=nome[j];
                 nome[j]=aux;
-
-    Io.println("Estes foram os nomes digitados e agora ordenados: ");
+              }
+    IO.println("Estes foram os nomes digitados e agora ordenados: ");
     for (int cont=0;cont<nome.length;cont++)
-        IO.println("Nome[" + (cont) + "]: " + nome[cont]);
-}  
+         IO.println("Nome[" + (cont) + "]: " + nome[cont]);
+}         
