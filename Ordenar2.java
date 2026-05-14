b94ac74ff8a6bd46546d7cdc9c4a7b415589b172
ferrazciro <imblack122@gmail.com>
2026-05-14 01:05:10 +0000

    aula dia 13/05

---
+++ Ordenar2.java
@@ -0,0 +1,23 @@
+void main(){
+
+    var nome = new Strin[5];
+    String aux;
+
+    for (int cont=0;cont<nome.length;cont++)
+        nome[cont] = IO.readln("Digite o nome[" + cont + "]: " );
+
+    IO.println("Estes foram os nomes digitados: ");
+    for (int cont=0;cont<nome.length;cont++)
+        IO.println("Nome[" + (cont) + "]: " + nome[cont]);
+    //ROTINA DE ORDENAÇÃO
+    for (int i=0;i<nome.length-1;i++)
+        for (int j=i+1;j<nome.length;j++)
+            if (nome[i].compareToIgnoreCase(nome[j]) > 0){
+                aux = nome[i];
+                nome[i]=nome[j];
+                nome[j]=aux;
+
+    Io.println("Estes foram os nomes digitados e agora ordenados: ");
+    for (int cont=0;cont<nome.length;cont++)
+        IO.println("Nome[" + (cont) + "]: " + nome[cont]);
+}  
